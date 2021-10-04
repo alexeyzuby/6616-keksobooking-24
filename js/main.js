69817b48111913029071337da9b5d68fa1e9fc1d
Aleksei Zubarev <alexeyzuby@bk.ru>
2021-10-04 10:33:35 +0300

    Fixed getRandomFloat. A string was returned.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomInt( min, max ) {
+const getRandomInt = ( min, max ) => {
   if( min < 0 || max < 0 ) {
     throw new Error( 'values should not be lower than 0' );
   }
@@ -8,9 +8,9 @@ function getRandomInt( min, max ) {
   }
 
   return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
-}
+};
 
-function getRandomFloat( min, max, fractionDigits ) {
+const getRandomFloat = ( min, max, fractionDigits ) => {
   if( min < 0 || max < 0 ) {
     throw new Error( 'values should not be lower than 0' );
   }
@@ -19,8 +19,8 @@ function getRandomFloat( min, max, fractionDigits ) {
     throw new Error( 'the max value should not be lower or equal than the min' );
   }
 
-  return ( Math.random() * ( max - min ) + min ).toFixed( fractionDigits );
-}
+  return parseFloat( ( Math.random() * ( max - min ) + min ).toFixed( fractionDigits ) );
+};
 
 getRandomInt( 1, 10 );
 getRandomFloat( 1, 10, 5 );
