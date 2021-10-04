073592dc792582793998e11b663d8335edcfdbe3
Aleksei Zubarev <alexeyzuby@bk.ru>
2021-10-04 10:08:57 +0300

    Added random functions

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function getRandomInt( min, max ) {
+  if( min < 0 || max < 0 ) {
+    throw new Error( 'values should not be lower than 0' );
+  }
+
+  if( max <= min ) {
+    throw new Error( 'the max value should not be lower or equal than the min' );
+  }
+
+  return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+}
+
+function getRandomFloat( min, max, fractionDigits ) {
+  if( min < 0 || max < 0 ) {
+    throw new Error( 'values should not be lower than 0' );
+  }
+
+  if( max <= min ) {
+    throw new Error( 'the max value should not be lower or equal than the min' );
+  }
+
+  return ( Math.random() * ( max - min ) + min ).toFixed( fractionDigits );
+}
+
+getRandomInt( 1, 10 );
+getRandomFloat( 1, 10, 5 );
